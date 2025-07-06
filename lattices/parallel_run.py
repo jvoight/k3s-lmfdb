ddf2fddfd31bc466baa00d9a183c5cb5a026d370
Eran Assaf <assaferan@gmail.com>
2025-07-06 13:16:39 +0000

    fixing tasks

--- lattices/parallel_run.py
+++ lattices/parallel_run.py
@@ -16,7 +16,8 @@ def calculate(func, args):
         func.__name__, args, result
         )
 
-tasks = [[(write_all_of_sig_between, (100*i+1, 100*(i+1), sig[0], sig[1])) for i in range(10)] for sig in all_sigs]
+tasks = [[(write_all_of_sig_between, (sig[0], sig[1], 100*i+1, 100*(i+1))) for i in range(10)] for sig in all_sigs]
 all_tasks = reduce(lambda x,y : x + y, tasks)
 my_pool = Pool(num_cpus)
 results = [my_pool.apply_async(calculate, t) for t in all_tasks]
+my_pool.close()
